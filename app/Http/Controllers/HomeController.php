c0d33127163308c1fdb720b9bbd884bfad2e7912
stygiansabyss <stygian.warlock.v2@gmail.com>
2017-04-13 02:03:38 -0500

    Removing test code from home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,15 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Apis\OpenDota\Players;
-use Syntax\SteamApi\Facades\SteamApi;
-
 class HomeController extends BaseController
 {
     public function index()
     {
-        $openDota = app(Players::class);
-        dd($openDota->getPlayer(76561198049801103));
         return $this->view();
     }
 }
